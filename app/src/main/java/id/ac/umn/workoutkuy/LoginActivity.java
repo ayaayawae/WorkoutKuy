403c8d3339ca1a7fbd9249063f0a13998f387caa
Bonaventura Sanjaya <bonaventura.sanjaya@student.umn.ac.id>
2021-11-07 18:39:16 +0700

    login

--- app/src/main/java/id/ac/umn/workoutkuy/LoginActivity.java
+++ app/src/main/java/id/ac/umn/workoutkuy/LoginActivity.java
@@ -2,13 +2,37 @@ package id.ac.umn.workoutkuy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class LoginActivity extends AppCompatActivity {
+    private boolean showPassword = false;
+    public EditText etPass;
+    public ImageView eyeIcon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        etPass = findViewById(R.id.etPassword);
+        eyeIcon = findViewById(R.id.show_pass_btn);
+    }
+
+    public void ShowHidePass(View view) {
+        if(!showPassword){
+            showPassword = !showPassword;
+            ((ImageView) eyeIcon).setImageResource(R.drawable.visibility);
+            etPass.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+        } else {
+            showPassword = !showPassword;
+            eyeIcon.setImageResource(R.drawable.hidden);
+            etPass.setTransformationMethod(PasswordTransformationMethod.getInstance());
+        }
     }
 }
--- app/src/main/java/id/ac/umn/workoutkuy/MainActivity.java
+++ app/src/main/java/id/ac/umn/workoutkuy/MainActivity.java
@@ -2,6 +2,7 @@ package id.ac.umn.workoutkuy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
