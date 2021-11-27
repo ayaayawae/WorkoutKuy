4e03f11eefa62b87437217aa4db565c24d956957
filbert29 <fkhouwira70@gmail.com>
2021-11-27 09:57:24 +0700

    edit name profile

--- app/src/main/java/id/ac/umn/workoutkuy/ProfileFragment.java
+++ app/src/main/java/id/ac/umn/workoutkuy/ProfileFragment.java
@@ -1,16 +1,19 @@
 package id.ac.umn.workoutkuy;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
@@ -33,6 +36,7 @@ public class ProfileFragment extends Fragment {
     private TextView profileName;
     private Button logoutBtn;
     private CircleImageView profileImg;
+    private ImageView editIcon;
     FirebaseDatabase rootNode;
     DatabaseReference reference;
 
@@ -48,7 +52,9 @@ public class ProfileFragment extends Fragment {
         profileName = view.findViewById(R.id.profile_name);
         logoutBtn = view.findViewById(R.id.btnLogout);
         profileImg = view.findViewById(R.id.profile_image);
+        editIcon = (ImageView) view.findViewById(R.id.editIcon);
 
+        AlertDialog.Builder alert = new AlertDialog.Builder(getContext());
         GoogleSignInAccount signInAccount = GoogleSignIn.getLastSignedInAccount(getContext());
 
         if(signInAccount != null) {
@@ -86,6 +92,31 @@ public class ProfileFragment extends Fragment {
                 FirebaseAuth.getInstance().signOut();
                 Intent intent = new Intent(getContext(), MainActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        editIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final EditText edittext = new EditText(getContext());
+                alert.setTitle("Change profile");
+                alert.setMessage("Change my name");
+
+                alert.setView(edittext);
+
+                alert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        String editName = edittext.getText().toString();
+                        reference.child("name").setValue(editName);
+                    }
+                });
+
+                alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                    }
+                });
+
+                alert.show();
             }
         });
     }
